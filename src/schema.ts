63228f15ef132fbec83b591f5ee867ee18d3bde3
GeonHyeok-Lee <dlrjsgur1992@gmail.com>
2019-09-14 21:18:59 +0900

    update better

--- src/schema.ts
+++ src/schema.ts
@@ -2,9 +2,11 @@ import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 import path from "path";
 
-const allTypes = fileLoader(path.join(__dirname, "./api/**/*.graphql"));
+const allTypes = fileLoader(path.join(__dirname, "./api/**/**/*.graphql"));
 
-const allResolvers = fileLoader(path.join(__dirname, "./api/**/*.resolvers.*"));
+const allResolvers = fileLoader(
+  path.join(__dirname, "./api/**/**/*.resolvers.*")
+);
 
 const mergedTypes = mergeTypes(allTypes);
 const mergedResolvers = mergeResolvers(allResolvers);
